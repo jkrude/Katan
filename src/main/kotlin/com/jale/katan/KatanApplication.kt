6c5335b11f533c958a95955295e804832456529c
lennart.thiesen <lennart.thiesen@stud.uni-goettingen.de>
2022-02-09 17:56:45 +0100

    changed package name

--- src/main/kotlin/com/jale/katan/KatanApplication.kt
+++ src/main/kotlin/com/jale/katan/KatanApplication.kt
@@ -1,4 +1,4 @@
-package com.jkth.katan
+package com.jale.katan
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
--- src/test/kotlin/com/jale/katan/KatanApplicationTests.kt
+++ src/test/kotlin/com/jale/katan/KatanApplicationTests.kt
@@ -1,4 +1,4 @@
-package com.jkth.katan
+package com.jale.katan
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
